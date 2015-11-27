b146abc85d31bb596fc6b43657e760c07bfb9cc2
Eli Mallon <eli.mallon@csats.com>
2015-11-27 03:10:02 -0600

    properly change home directory

--- init-env.fish
+++ init-env.fish
@@ -1,6 +1,10 @@
 #!/usr/bin/fish
 chsh -s /usr/bin/fish
 mkdir /home/root
-usermod -m -d /home/root root
+
+# Can't change root's home because we're root... so change /etc/passwd instead.
+cat /etc/passwd | sed s/"root:\/root"/"root:\/home\/root"/ > /tmp/newpasswd
+cat /tmp/newpasswd > /etc/passwd
+rm /tmp/newpasswd
 
 locale-gen en_US.UTF-8
