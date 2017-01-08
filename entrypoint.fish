907e6e2c991e5dde409c3e473cdf9fb2947df161
Eli Mallon <eli@stream.kitchen>
2017-01-07 19:41:30 -0800

    update /root directory

--- entrypoint.fish
+++ entrypoint.fish
@@ -1,20 +1,20 @@
 #!/usr/bin/fish
 
 # Generate an SSH key at runtime
-if not test -e /root/.ssh/id_rsa
+if not test -e /home/root/.ssh/id_rsa
   ssh-keygen -b 4096 -t rsa -N "" -f ~/.ssh/id_rsa >/dev/null 2>&1
 end
 
-if not test -L /root/.config/fish
-  mkdir -p /root/code
-  cd /root/code
+if not test -L /home/root/.config/fish
+  mkdir -p /home/root/code
+  cd /home/root/code
   git clone https://github.com/iameli/home.git
-  rm -rf /root/.config/fish
-  ln -s /root/code/home/fish /root/.config/fish
+  rm -rf /home/root/.config/fish
+  ln -s /home/root/code/home/fish /home/root/.config/fish
 end
 
 set -Ux LC_ALL en_US.utf8
-set -Ux my_emoji 🍗
+set -Ux my_emoji⭐️
 set -Ux fish_greeting "Bwaaaaaaak!"
 
 # The internet tells me this line tells me whether I'm interactive. Cool!
